31ab8b3dbf61a9e826d84df5f24bd141176aaf87
DVD <85802704+DVolodyslavD@users.noreply.github.com>
2021-06-18 20:51:57 +0300

    Update sq_statement.py

--- sq_statement.py
+++ sq_statement.py
@@ -2,7 +2,7 @@ import sqlite3 as sq
 
 
 # List of all users
-def select_users(db_file):
+def select_users(db_file: str) -> tuple:
     connect = sq.connect(db_file)
     cur = connect.cursor()
     cur.execute("SELECT user_id FROM all_users")
@@ -12,7 +12,7 @@ def select_users(db_file):
 
 
 # User entry into the database
-def write_user(db_file, user_id, first_name, username):
+def write_user(db_file: str, user_id: int, first_name: str, username: str) -> None:
     connect = sq.connect(db_file)
     cursor = connect.cursor()
     cursor.execute("INSERT INTO all_users (user_id, first_name, username) VALUES (?, ?, ?)", (user_id, first_name,
